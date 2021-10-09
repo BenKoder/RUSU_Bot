8c00f14128c10f1bdcf43f9eedb0a2e881e044dc
Ben Koder <s3774343@student.rmit.edu.au>
2021-10-09 22:11:33 +1100

    Initial commit

---
+++ main.js
@@ -0,0 +1,34 @@
+const { Client, Intents } = require('discord.js');
+const { token, guildID, verifiedID } = require('./config.json');
+const express = require('express')
+const app = express()
+
+// Create a new client instance
+const myIntents = new Intents();
+myIntents.add(Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS);
+const client = new Client({ intents: myIntents });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+    console.log('Ready!');
+
+    //Lets the web server parse JSON files
+    app.use(express.json())
+
+//Accepts post requests to the /verify URL
+    app.post('/verify', async (req, res) => {
+        console.log(req.body)
+        res.end()
+        const guild = await client.guilds.fetch(guildID)
+        const allMembers = await guild.members.fetch({cache: false})
+        const member = allMembers.find(member => member.user.tag === req.body.discordtag)
+        if(!member) return
+        await member.roles.add(verifiedID)
+        member.send ("You have been successfully verified!")
+    })
+
+    app.listen(8080)
+});
+
+// Login to Discord with your client's token
+client.login(token);
