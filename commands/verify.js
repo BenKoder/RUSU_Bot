f0aa4f6e20661e50cee6c904a8586396e743c4b1
Ben Koder <s3774343@student.rmit.edu.au>
2021-10-10 13:41:59 +1100

    Slash commands actually work

--- commands/verify.js
+++ commands/verify.js
@@ -2,9 +2,9 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 //verify module
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('verifylink')
+        .setName('verify')
         .setDescription('Get the link to be verified'),
     async execute(interaction) {
-        return interaction.reply({content: 'Complete this form to get verified: https://forms.office.com/r/UBKCGPwfEF', ephemeral: true});
+        return interaction.reply({content: 'Complete this form to get verified:\nhttps://forms.office.com/r/UBKCGPwfEF', ephemeral: true});
     },
 };
